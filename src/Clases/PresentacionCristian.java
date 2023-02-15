3c762ee90c39072b2fbe79e15cccc6f745f6ee8d
crifero <crodriguezobando14@gmail.com>
2023-02-15 18:54:20 -0500

    Presentacion cristian rodriguez

---
+++ src/Clases/PresentacionCristian.java
@@ -0,0 +1,9 @@
+package Clases;
+
+public class PresentacionCristian {
+    public static void main(String[] args) {
+        System.out.println("Cristian felipe rodriguez obando");
+        System.out.println("decimo semestre mas o menos xd");
+        System.out.println("jugar play");
+    }
+}
