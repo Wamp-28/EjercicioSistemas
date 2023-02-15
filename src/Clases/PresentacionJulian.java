5161a1c543feef10bb1486f32ac2c2021c91400e
Julian <yandavid2000@gmail.com>
2023-02-15 18:55:43 -0500

    Presentacion mia

---
+++ src/Clases/PresentacionJulian.java
@@ -0,0 +1,10 @@
+package Clases;
+
+public class PresentacionJulian {
+
+    public static void main(String[] args) {
+        System.out.println("Julian Jaramillo");
+        System.out.println("Decimo");
+        System.out.println("Ver deportes");
+    }
+}
