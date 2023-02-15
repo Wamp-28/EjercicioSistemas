d439d49d0656241927a0c43b8046d504dd4e46c2
camilo <kmilod7@gmail.com>
2023-02-15 18:57:36 -0500

    presentacion

--- src/Clases/Presentacion_Camilo.java
+++ src/Clases/Presentacion_Camilo.java
@@ -1,2 +1,9 @@
-package Clases;public class Presentacion {
+package Clases;
+
+public class Presentacion_Camilo {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Camilo Diaz");
+        System.out.println("Ultimo Semestre");
+        System.out.println("Mi hobbie es jugar futbol los fines de semana");
+    }
 }
