c72d3656d505501ce195394b330c50b62b4b6460
William Alexander Matallana <wmatallana28@gmail.com>
2023-02-17 19:43:02 -0500

    Ajuste ramas

--- src/Clases/PresentacionCristian.java
+++
@@ -1,9 +0,0 @@
-package Clases;
-
-public class PresentacionCristian {
-    public static void main(String[] args) {
-        System.out.println("Cristian felipe rodriguez obando");
-        System.out.println("decimo semestre mas o menos xd");
-        System.out.println("jugar play");
-    }
-}
--- src/Clases/Presentacion_Camilo.java
+++
@@ -1,9 +0,0 @@
-package Clases;
-
-public class Presentacion_Camilo {
-    public static void main(String[] args) {
-        System.out.println("Mi nombre es Camilo Diaz");
-        System.out.println("Ultimo Semestre");
-        System.out.println("Mi hobbie es jugar futbol los fines de semana");
-    }
-}
